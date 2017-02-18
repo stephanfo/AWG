ef54e2ea365e336e7d005ffeb5647e9503b4a2e7
Stéphane RATELET <stephanfo@users.noreply.github.com>
2017-02-18 15:42:38 +0100

    Delete todo.php

--- todo.php
+++
@@ -1,7 +0,0 @@
-<?php
-//
-// TODO: Define credit to user (voucher)
-// TODO: Create a view with "most liked photos" and "current orders status"
-// TODO: Generate the JSX files from orders to be able to ease the printing in Adobe Photoshop
-// TODO: Add multilingual functionality
-// TODO: Add settings for messages and other personal area
